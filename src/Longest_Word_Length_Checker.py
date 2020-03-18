4536796c675ef144c7f7cfd28061667f2c70023e
Mrs Tunde Lawal <jessanthon@gmail.com>
2020-03-18 12:22:11 +0000

    Added shortest_word_checker method

--- src/Longest_Word_Length_Checker.py
+++ src/Longest_Word_Length_Checker.py
@@ -1,11 +1,19 @@
 class Longest_Word_Length_Checker:
 
     def longest_word_checker(self,sentence):
-
         longest_word = ''
         for word in sentence.split(' '):
             if len(word) >= len(longest_word):
                 longest_word = word
                 longest_length = len(longest_word)
-        a = [longest_length, longest_word]
-        return a
+        long_word = [longest_length, longest_word]
+        return long_word
+
+    def shortest_word_checker(self, sentence):
+        shortest_word = sentence
+        for word in sentence.split(' '):
+            if len(word) < len(shortest_word):
+                shortest_word = word
+                shortest_length = len(shortest_word)
+        short_word = [shortest_length, shortest_word]
+        return short_word
